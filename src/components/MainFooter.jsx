3b15a3c1a7204cceff4c39130d67598dc5c57a3e
Alessandro Ledda <aleledda75@icloud.com>
2026-01-22 09:19:12 +0100

    fin e refactor in file mainfooter.jsx

--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -1,71 +1,105 @@
 const MainFooter = () => {
+
+    const linkFooter = [
+        {
+            title: "DC COMICS",
+            link: [
+                { id: 1, label: "Characters", url: "#", },
+                { id: 2, label: "Comics", url: "#", },
+                { id: 3, label: "Movies", url: "#", },
+                { id: 4, label: "Tv", url: "#", },
+                { id: 5, label: "Games", url: "#", },
+                { id: 6, label: "Videos", url: "#", },
+                { id: 7, label: "News", url: "#", },
+            ],
+        },
+        {
+            title: "SHOP",
+            link: [
+                { id: 8, label: "Shop DC", url: "#", },
+                { id: 9, label: "Shop DC Collectible", url: "#", },
+
+            ],
+        },
+
+        {
+            title: "DC",
+            link: [
+                { id: 10, label: "Terms Of Use", url: "#", },
+                { id: 11, label: "Privacy Policy (new)", url: "#", },
+                { id: 12, label: "Ad Choices", url: "#", },
+                { id: 13, label: "Advertising", url: "#", },
+                { id: 14, label: "Jobs", url: "#", },
+                { id: 15, label: "Subscription", url: "#", },
+                { id: 16, label: "Talent Worckshop", url: "#", },
+                { id: 17, label: "CPSC Certificates", url: "#", },
+                { id: 18, label: "Ratings", url: "#", },
+                { id: 19, label: "Shop Help", url: "#", },
+                { id: 20, label: "Contact Us", url: "#", },
+            ],
+        },
+        {
+            title: "SITES",
+            link: [
+                { id: 21, label: "DC", url: "#", },
+                { id: 22, label: "MAD Magazine", url: "#", },
+                { id: 23, label: "DC Kids", url: "#", },
+                { id: 24, label: "DC Universe", url: "#", },
+                { id: 25, label: "DC Power Visa", url: "#", },
+            ],
+        }
+
+    ]
+
+    // funzione che renderezza i link con le rispettive informazioni
+    function renderLinks(arrayLink) {
+        return arrayLink.map((elArr) => {
+            return (
+                <li key={elArr.id}>
+                    <a href={elArr.url}>{elArr.label}</a>
+                </li>
+            )
+        })
+    }
+
+
+
     return (
         <footer>
             <section className="container-footer">
                 <div className="content-link-img">
                     <div className="wrap first-footer">
                         <div className="link-footer">
-                            <div className="col-one">
-                                <h4>DC COMICS</h4>
-                                <ul>
-                                    <li><a href="">Characters</a></li>
-                                    <li><a href="">Comics</a></li>
-                                    <li><a href="">Movies</a></li>
-                                    <li><a href="">Tv</a></li>
-                                    <li><a href="">Games</a></li>
-                                    <li><a href="">Videos</a></li>
-                                    <li><a href="">News</a></li>
-                                </ul>
-                                <h4>SHOP</h4>
-                                <ul>
-                                    <li><a href="">Shop DC</a></li>
-                                    <li><a href="">Shop DC Collectibles</a></li>
-                                </ul>
-                            </div>
-                            <div className="col-two">
-                                <h4>DC</h4>
-                                <ul>
-                                    <li><a href="">Term of use</a></li>
-                                    <li><a href="">Privacy policy (new)</a></li>
-                                    <li><a href="">Ad Choise</a></li>
-                                    <li><a href="">Advertaising</a></li>
-                                    <li><a href="">Jobs</a></li>
-                                    <li><a href="">Subscription</a></li>
-                                    <li><a href="">Talent Workshops</a></li>
-                                    <li><a href="">CPSC Certificates</a></li>
-                                    <li><a href="">Ratings</a></li>
-                                    <li><a href="">Shop Help</a></li>
-                                    <li><a href="">Contact Us</a></li>
-                                </ul>
-                            </div>
-                            <div className="col-three">
-                                <h4>SITES</h4>
-                                <ul>
-                                    <li><a href="">DC</a></li>
-                                    <li><a href="">MAD Megazine</a></li>
-                                    <li><a href="">DC kids</a></li>
-                                    <li><a href="">DC Universe</a></li>
-                                    <li><a href="">DC Power Visa</a></li>
-                                </ul>
-                            </div>
+
+                            {linkFooter.map((footerLk) => (
+                                <div className="col-one" ky={footerLk.id}>
+                                    <h4>{footerLk.title}</h4>
+                                    <ul>
+                                        {/* richiamo la funzione render */}
+                                        {renderLinks(footerLk.link)}
+                                    </ul>
+                                </div>
+                            ))}
+
+
                         </div>
                         <div className="img-footer">
                             <img src="./src/assets/imgs/dc-logo-bg.png" alt="" />
                         </div>
                     </div>
-                </div>
-                <div className="content-sign-follow">
-                    <div className="wrap">
-                        <div className="sign-up">
-                            <button className="btn"><a href="">SIGN-UP NOW!</a></button>
-                        </div>
-                        <div className="follow-us">
-                            <h3>FOLLOWS US</h3>
-                            <img src="./src/assets/imgs/footer-facebook.png" alt="" />
-                            <img src="./src/assets/imgs/footer-twitter.png" alt="" />
-                            <img src="./src/assets/imgs/footer-youtube.png" alt="" />
-                            <img src="./src/assets/imgs/footer-pinterest.png" alt="" />
-                            <img src="./src/assets/imgs/footer-periscope.png" alt="" />
+                    <div className="content-sign-follow">
+                        <div className="wrap">
+                            <div className="sign-up">
+                                <button className="btn"><a href="">SIGN-UP NOW!</a></button>
+                            </div>
+                            <div className="follow-us">
+                                <h3>FOLLOWS US</h3>
+                                <img src="./src/assets/imgs/footer-facebook.png" alt="" />
+                                <img src="./src/assets/imgs/footer-twitter.png" alt="" />
+                                <img src="./src/assets/imgs/footer-youtube.png" alt="" />
+                                <img src="./src/assets/imgs/footer-pinterest.png" alt="" />
+                                <img src="./src/assets/imgs/footer-periscope.png" alt="" />
+                            </div>
                         </div>
                     </div>
                 </div>
